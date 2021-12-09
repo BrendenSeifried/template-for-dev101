f8165beeaff5079ba9a2a415f594cb8c7201a015
BrendenSeifried <brendenseifried@gmail.com>
2021-12-09 12:00:25 -0800

    fixed-2

--- test/tests.js
+++ test/tests.js
@@ -21,3 +21,35 @@ test('player rock, computer rock is tie', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('player rock, computer scissors is tie', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const player = 'rock';
+    const computer = 'scissors';
+    const expected = 'player';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = score(player, computer);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('player rock, computer scissors is tie', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const player = 'rock';
+    const computer = 'paper';
+    const expected = 'computer';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = score(player, computer);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
