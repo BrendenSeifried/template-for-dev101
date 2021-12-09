407982ebf01292b6f2dd203cb48f81b79191951c
BrendenSeifried <brendenseifried@gmail.com>
2021-12-09 11:43:30 -0800

    2

--- test/game.js
+++ test/game.js
@@ -2,5 +2,29 @@
 export function score(player, computer) {
     if (player === 'rock' && computer === 'rock') {
         return 'tie';
+    } 
+    if (player === 'rock' && computer === 'scissors') {
+        return 'player';
+    }
+    if (player === 'rock' && computer === 'paper') {
+        return 'computer';
+    }
+    if (player === 'scissors' && computer === 'paper') {
+        return 'player';
+    }
+    if (player === 'scissors' && computer === 'rock') {
+        return 'computer';
+    }
+    if (player === 'scissors' && computer === 'scissors') {
+        return 'tie';
+    }
+    if (player === 'paper' && computer === 'paper') {
+        return 'tie';
+    }
+    if (player === 'paper' && computer === 'rock') {
+        return 'player';
+    }
+    if (player === 'paper' && computer === 'scissors') {
+        return 'computer';
     }
 }
