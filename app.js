d97a3df8694473e4b72e85319191be2def469ef0
BrendenSeifried <brendenseifried@gmail.com>
2021-12-09 10:19:43 -0800

    first

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 // grab DOM elements
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+    // get user input
+    // use user input to update state 
+    // update DOM to reflect the new state
+
+console.log('missing semi-colon');
+
+const x = 3;
+
+if (x === 3) {
+console.log(x);
+}
